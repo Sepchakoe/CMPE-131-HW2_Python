e05981110b317c93e4d4171380c73a2fa45719bd
Koe <98497607+Sepchakoe@users.noreply.github.com>
2022-11-12 20:18:19 -0800

    Update swap.py

--- swap.py
+++ swap.py
@@ -4,12 +4,14 @@ CMPE 131
 HW2a Python Q2
 """
 
-def swapList(my_list):
-  if len(my_list) % 2 == 0:
-    middleIndex = (len(my_list) // 2) - 1
+def swap_list(my_list):
+  #if len(my_list) % 2 == 0:
+    middleIndex = (len(my_list)-1) // 2
     my_list[-1], my_list[middleIndex] = my_list[middleIndex], my_list[-1]
     return my_list
+  """
   else:
     middleIndex = (len(my_list)) // 2
     my_list[-1], my_list[middleIndex] = my_list[middleIndex], my_list[-1]
     return my_list
+    """
