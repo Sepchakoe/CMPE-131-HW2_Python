d47042f6da4bf591b9a58508fc4995587afe7b72
Koe <98497607+Sepchakoe@users.noreply.github.com>
2022-11-12 22:59:18 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,20 +5,32 @@ HW2a Python Q3
 """
 
 def sort_dictionary(inputDict):
-    dictByAge = {}
-    result = []
-    # to create a new dictionary, age is the key, (name , phone) is the value
+    i = 0
+    ageArr = []
+    nameArr = []
+    phNumArr = []
+    
     for key in inputDict:
         name = key
         phoneNumber = inputDict[key][0]
         age = inputDict[key][1]
-        if age not in dictByAge:
-            dictByAge[age] = [(name, phoneNumber)]
-        else:
-            dictByAge[age].append((name, phoneNumber))
-            
-    #get the value after sorted the dictionary by key (age)
-    for a in sorted(dictByAge.keys()) :
-        result += dictByAge[a]
-    return result
- 
+        
+        #adding into separte arraylist
+        ageArr.append(age)
+        nameArr.append(name)
+        phNumArr.append(phoneNumber)
+        
+    #start soring the arraylist by age
+    i = 0
+    while i < len(inputDict):
+        k = 0
+        while k < i:
+            if (ageArr[i] < ageArr[k]):
+                ageArr[k], ageArr[i] = ageArr[i], ageArr[k]
+                nameArr[k], nameArr[i] = nameArr[i], nameArr[k]
+                phNumArr[k], phNumArr[i] = phNumArr[i], phNumArr[k]
+            k += 1
+        i += 1
+        
+    tupleList = [(nameArr[i], phNumArr[i]) for i in range(0, len(inputDict))]
+    return tupleList
